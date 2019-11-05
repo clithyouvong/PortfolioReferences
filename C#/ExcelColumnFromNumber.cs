8628c9c5ea59a50ba5502a11a72422dae9d4060e
Colby Lithyouvong <clithyouvong@users.noreply.github.com>
2019-11-05 14:58:56 -0500

    Update ExcelColumnFromNumber.cs

--- C#/ExcelColumnFromNumber.cs
+++ C#/ExcelColumnFromNumber.cs
@@ -1,1 +1,32 @@
+  
+/*
+  The following is to be used for reference only. 
+  
+  The following implementation is not used in any implemenation for any company. 
+  
+  The following is written in C# 
+  
+  The following returns a string representing an Excel Column
+*/
 
+    /// <summary>
+    /// 1 -> A<br/>
+    /// 2 -> B<br/>
+    /// 3 -> C<br/>
+    /// ...
+    /// </summary>
+    /// <param name="column"></param>
+    /// <returns></returns>
+    public static string ExcelColumnFromNumber(int column)
+    {
+        string columnString = "";
+        decimal columnNumber = column;
+        while (columnNumber > 0)
+        {
+            decimal currentLetterNumber = (columnNumber - 1) % 26;
+            char currentLetter = (char)(currentLetterNumber + 65);
+            columnString = currentLetter + columnString;
+            columnNumber = (columnNumber - (currentLetterNumber + 1)) / 26;
+        }
+        return columnString;
+    }
