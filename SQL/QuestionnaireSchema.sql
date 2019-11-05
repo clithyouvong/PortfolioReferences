279f8103c3fb9270e568a22b32865c13d53037e2
Colby Lithyouvong <clithyouvong@users.noreply.github.com>
2019-11-05 09:47:45 -0500

    Create QuestionnaireSchema.sql

---
+++ SQL/QuestionnaireSchema.sql
@@ -0,0 +1,7 @@
+/*
+  This is the Questionnaire System used to simulate an online Exam or Survey System.
+  
+  The following code demonstrates SQL interpretations used in SQL Azure 2016.
+  
+  This is not a copy of any current implementation of any company but rather a demonstration of how things worked. 
+*/
