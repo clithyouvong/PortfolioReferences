fba23c6de023000d3310df2e4a0f1c77b0eb3588
Colby Lithyouvong <clithyouvong@users.noreply.github.com>
2020-01-02 18:52:36 -0500

    tablestoragedemo

---
+++ PluralSight/TableStorageDemo/Models/TodoModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TableStorageDemo.Models
+{
+    public class TodoModel
+    {
+        public string Id { get; set; }
+        public string Group { get; set; }
+        public string Content { get; set; }
+        public string DueDate { get; set; }
+        public bool Completed { get; set; }
+
+        public DateTimeOffset Timestamp { get; set; }
+        public DateTime? CompletedDate { get; set; }
+    }
+}
