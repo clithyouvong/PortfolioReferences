effa3b2b1b93cda06c3dbe8a895d65c5121e0da0
Colby Lithyouvong <clithyouvong@users.noreply.github.com>
2021-12-08 15:27:00 -0500

    Update Get-AzConsumptionUsageDetail.ps1

--- PowerShell/Get-AzConsumptionUsageDetail.ps1
+++ PowerShell/Get-AzConsumptionUsageDetail.ps1
@@ -1,17 +1,33 @@
 <#
-  .SYNOPSIS
-  Gets the Azure Consumption Usage Detail and output's it to a csv file
+.SYNOPSIS
+    Gets the Azure Consumption Usage Detail and output's it to a csv file
 
-  .DESCRIPTION
-  Gets the Azure Consumption Usage Detail and output's it to a csv file
-  
-  .Author
-  Colby Lithyouvong
+.DESCRIPTION
+    This uses Get-AzConsumptionUsageDetail, grabs only relevant information, and outputs it to a csv file on the desktop. this assumes to be run from local.
+
+.PARAMETER StartDate
+    the date to begin the scan for az consumption usage
+
+.PARAMETER EndDate
+    the date to end the scan for az consumption usage
+
+.EXAMPLE
+    The output finishes in a few minutes with an output on the local desktop
+    PS C:\> .\Get-AzConsumptionUsageDetail.ps1 -StartDate "2021-01-01" -EndDate "2021-01-01"
+
+.NOTES
+    Author: Colby Lithyouvong
+    Last Edit: 2021-12-06
+    Version 1.0 - initial release
 
 #>
 
-$StartDate = "2021-01-01"
-$EndDate = "2021-01-31"
+param {
+  [string] $StartDate = "2021-01-01",
+  
+  [string] $EndDate = "2021-01-31"
+}
+
 
 function GetDateTimeNow(){
     return Get-Date -Format 'yyyyMMdd-hhmmss'
